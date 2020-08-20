5a6939c63905040c904abf9841e1156e710c7c70
vaddv <vijay.vaddem@gmail.com>
2020-08-20 10:37:59 +0530

    refactored comments

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,11 +14,11 @@ app.listen("4001", () => {
 
 const commentsByPostID = {};
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { commentText } = req.body;
 
-  console.log("Comment content received to svc", commentText);
+  //console.log("Comment content received to svc", commentText);
   //get the existing comments for a given post.
   const comments = commentsByPostID[req.params.id] || [];
 
@@ -30,9 +31,25 @@ app.post("/posts/:id/comments", (req, res) => {
   //add it back to original array
   commentsByPostID[req.params.id] = comments;
 
+  //post to event bus
+  await axios.post("http://localhost:4005/events", {
+    type: "commentCreated",
+    data: {
+      id: comments,
+      commentText,
+      documentId: req.params.id,
+    },
+  });
+
   res.status(201).send(comments);
 });
 
 app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostID[req.params.id] || []);
+});
+
+//post handler for incoming events request
+app.post("/event", (req, res) => {
+  console.log("Incoming event received for comment:", req.body.type);
+  res.send({});
 });
